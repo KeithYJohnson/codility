de855db0d5fac9c11fc512ad4728169531ede1e9
Keith Johnson <keithyjohnson@gmail.com>
2017-04-23 12:37:15 -0400

    cyclic rotations in python

---
+++ cyclic_rotation/py_run.py
@@ -0,0 +1,18 @@
+# https://codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+
+def solution(a, num_rotations):
+    rotated_a = [None] * len(a)
+    for idx, e in enumerate(a):
+        new_idx = (idx + num_rotations) % len(a)
+        rotated_a[new_idx] = e
+
+    return rotated_a
+
+
+A = [3, 8, 9, 7, 6]
+print(solution(A, 1))
+
+A = [3, 8, 9, 7, 6]
+K = 3
+
+print(solution(A, K))
