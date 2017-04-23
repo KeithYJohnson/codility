3618bdb5264386945c2c5e0d8eafb76badc264a3
Keith Johnson <keithyjohnson@gmail.com>
2017-04-23 12:21:42 -0400

    links to other nice solutions to odd occurences in array

--- odd_occurences/py_run.py
+++ odd_occurences/py_run.py
@@ -13,3 +13,6 @@ def solution(a):
 
 a = [9,3,9,3,9,7,9]
 print(solution(a))
+
+# Here's a nice ruby solution, though technically a bit slower - https://github.com/plicjo/codility_exercises/blob/master/odd_occurrences_in_array.rb
+# ooooooo and a slick XOR solution is here - https://www.martinkysel.com/codility-oddoccurrencesinarray-solution/
